6e7a449214d9a6c89f2609b510fcbcf3601085d7
Ting Hao Chang <tinghaothc@gmail.com>
2019-04-13 17:54:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/expressid/Homepage/HomepageActivity.java
@@ -0,0 +1,15 @@
+package com.example.expressid.Homepage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.expressid.R;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
